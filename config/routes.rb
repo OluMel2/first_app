3aab579e5f16e28bc622be0b61d05600f3448a71
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-09-24 13:16:02 +0000

    [復習]説明文追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #ルーティング(Routing)では、ユーザが特定のURLにアクセスしたときに、どのコントローラのどのアクションに処理を振り分けるかを定義します。 ルーティングは、URLとアクションを結びつける役目をしています。
+  #たとえば今回は、「'top'というURLにアクセスがあったときに、homesコントローラのtopアクションの処理を呼び出す」定義を、ルーティングに記述します。
+  get 'top' => 'homes#top'
 end
