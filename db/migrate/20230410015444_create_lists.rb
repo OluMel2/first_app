715788d383030d97aeb7f26b6c2e38694ebe5e72
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-09-25 14:37:46 +0000

    [Update]migrate file schema.rb

--- db/migrate/20230410015444_create_lists.rb
+++ db/migrate/20230410015444_create_lists.rb
@@ -5,6 +5,9 @@ class CreateLists < ActiveRecord::Migration[6.1]
       t.string :body
       t.string :author
       t.timestamps
+      #t.データ型 :カラム名
     end
   end
 end
+
+#migrationファイルはモデルを作成した際に自動生成されているので、あとは編集するだけです。
